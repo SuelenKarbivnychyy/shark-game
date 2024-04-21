f65b9a8ba3d60e2b5201db6e90a05a18ef495f35
Suelen Karbivnychyy <suelenmatosr@gmail.com>
2024-04-21 18:23:11 -0400

    Implemened functions to build UI for shark game

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -22,7 +22,19 @@ let correctGuesses = 0;
 // You can change this to choose a random word from WORDS once you
 // finish this lab but we hard code it so we know what the word is
 // and can tell if things look correct for this word
-const word = 'hello';
+
+
+const wordToGuess = (listOfWords) => {
+
+  const randomIndex = Math.floor(Math.random() * listOfWords.length);
+
+  const word = listOfWords[randomIndex];
+
+  return word;
+
+}
+
+const word = wordToGuess(WORDS);
 
 // Loop over the letters in `word` and create divs.
 // The divs should be appended to the section with id="word-container".
@@ -31,13 +43,23 @@ const word = 'hello';
 // `<div class="letter-box ${letter}"></div>`
 //
 function createDivsForChars() {
-  // Replace this with your code
+  
+  for (const char of word) {
+    document.querySelector('#word-container').insertAdjacentHTML('beforeend', `<div class="letter-box ${char}"></div>`);
+  };
+
 }
+
 
 // Loop over each letter in the alphabet and generate a button for each letter
 // The buttons should be appended to the section with id="letter-buttons".
 function generateLetterButtons() {
-  // Replace this with your code
+  
+  const letterButtons = document.querySelector('#letter-buttons');
+
+  for (letter of ALPHABET) {
+    letterButtons.insertAdjacentHTML("beforeend", `<button>${letter}</button>`);
+  };
 }
 
 // Set the `disabled` property of `buttonEl` to true.
@@ -45,14 +67,20 @@ function generateLetterButtons() {
 // `buttonEl` is an `HTMLElement` object.
 //
 function disableLetterButton(buttonEl) {
-  // Replace this with your code
+  buttonEl.setAttribute('disabled', 'true');
+  
 }
 
 // This is a helper function we will use in the future
 // It should return `true` if `letter` is in the word
 // For now, you should test it out to make sure it works
 function isLetterInWord(letter) {
-  // Replace this with your code
+  
+  if (word.includes(letter)) {
+    return true;
+  } else {
+    return false;
+  };
 }
 
 // This function is called to start the game.
@@ -60,8 +88,16 @@ function startGame() {
   createDivsForChars();
   generateLetterButtons();
 
-  // in the next lab, you will be adding functionality to handle when
-  // someone clicks on a letter
+  const buttons = document.querySelectorAll('#letter-buttons button');
+
+  for (const button of buttons) {
+    // add an event handler that calls the buttonClicked function when a button is clicked
+    // YOUR CODE HERE
+
+  }
+
+  // add an event handler to handle clicking on the Play Again button
+  // YOUR CODE HERE
 }
 
 startGame(); // Call startGame() when the page loads.
